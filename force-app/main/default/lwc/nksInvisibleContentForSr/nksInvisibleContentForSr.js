3ef23fe5b93d3de99d55aac8947db91abc6dc1b8
Eivind Øksnevad <eivind.oksnevad@nav.no>
2022-11-04 10:34:49 +0100

    Add datasource

--- force-app/main/default/lwc/nksInvisibleContentForSr/nksInvisibleContentForSr.js
+++ force-app/main/default/lwc/nksInvisibleContentForSr/nksInvisibleContentForSr.js
@@ -9,16 +9,19 @@ export default class NksInvisibleContentForSr extends LightningElement {
     h4tag = false;
     h5tag = false;
     h6tag = false;
+    // the above tag-list is duplicated below, in .js-meta.xml, and in .html
 
     connectedCallback() {
-        if(this.tag == 'h1') this.h1tag = true;
-        else if(this.tag == 'h2') this.h2tag = true;
-        else if(this.tag == 'h3') this.h3tag = true;
-        else if(this.tag == 'h4') this.h4tag = true;
-        else if(this.tag == 'h5') this.h5tag = true;
-        else if(this.tag == 'h6') this.h6tag = true;
-        else {
-            throw new Error(`Tag not allowed here: <${this.tag}>`);
+        switch(this.tag) {
+            case 'h1': this.h1tag = true; break;
+            case 'h2': this.h2tag = true; break;
+            case 'h3': this.h3tag = true; break;
+            case 'h4': this.h4tag = true; break;
+            case 'h5': this.h5tag = true; break;
+            case 'h6': this.h6tag = true; break;
+            default:
+                throw new Error(`Tag not allowed here: <${this.tag}>`);
+                break;
         }
     }
 }
