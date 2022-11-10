9fb94fb3e1073589a15f3aa95ce4442b537245ff
Morten Mikalsen <morten.mikalsen@hotmail.com>
2022-11-10 09:56:39 +0100

    Adds filter to fagsystem component

--- force-app/main/default/lwc/nksFagsystemer/nksFagsystemer.js
+++ force-app/main/default/lwc/nksFagsystemer/nksFagsystemer.js
@@ -5,14 +5,39 @@ import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import PERSON_IDENT_FIELD from '@salesforce/schema/Person__c.Name';
 import { refreshApex } from '@salesforce/apex';
 
+const filterFunc = (listToFilterOut, listToFilterIn) => (element) => {
+    return (
+        (!listToFilterOut || !listToFilterOut.includes(element.name)) &&
+        (!listToFilterIn || listToFilterIn.includes(element.name))
+    );
+};
+
 export default class NksFagsystemer extends LightningElement {
     @api recordId;
     @api title;
     @api relatedField;
     @api objectApiName;
-    @track personId;
+    @api personId;
     @track showLinks;
     @track inFagsone = false;
+    @api filterList;
+
+    hiddenLinks = ['Aktivitetsplan', 'Speil'];
+
+    possibleLinks = [
+        { name: 'Modia', field: 'NKS_ModiaURL__c' },
+        { name: 'Gosys', field: 'NKS_GoSysURL__c' },
+        { name: 'Aktivitetsplan', field: 'NKS_AktivitetsplanURL__c' },
+        { name: 'AA-reg', field: null, eventFunc: this.handleAAClickOrKey, title: 'AA-register' },
+        { name: 'DinPensjon', field: 'NKS_PSELVPensjonURL__c' },
+        { name: 'DinUfore', field: 'NKS_PSELVUfoeretrygdURL__c' },
+        { name: 'Pesys', field: 'NKS_PESYSURL__c' },
+        { name: 'Speil', field: 'NKS_SpeilURL__c' },
+        { name: 'Foreldrepenger', field: 'NKS_ForeldrepengerURL__c' },
+        { name: 'K9', field: 'NKS_K9URL__c' },
+        { name: 'Barnetrygd', field: 'NKS_BarnetrygdURL__c' },
+        { name: 'Enslig', field: 'NKS_EnsligForsorgerURL__c' }
+    ];
 
     connectedCallback() {
         checkFagsoneIpRange().then((res) => {
@@ -21,6 +46,14 @@ export default class NksFagsystemer extends LightningElement {
                 console.log('Ip is: ' + res.ip);
             }
         });
+    }
+
+    renderedCallback() {
+        const listOfFilter =
+            typeof this.filterList === 'string' ? this.filterList.replaceAll(' ', '').split(',') : this.filterList;
+        this.fields = this.possibleLinks
+            .map((link, index) => ({ ...link, id: index, custom: link.field == null }))
+            .filter(filterFunc(this.hiddenLinks, listOfFilter));
     }
 
     get size() {
@@ -55,6 +88,10 @@ export default class NksFagsystemer extends LightningElement {
 
     get showContent() {
         return this.personId != null;
+    }
+
+    get showRefreshButton() {
+        return !(!this.objectApiName || !this.recordId || !this.relatedField);
     }
 
     handleAAClickOrKey(e) {
