77d8a3ffca07e9db159790c9a05c5f26dfb84117
Morten Mikalsen <morten.mikalsen@hotmail.com>
2023-02-07 12:09:31 +0100

    Remove Modia Sosial Link (again)

--- force-app/main/default/lwc/nksFagsystemer/nksFagsystemer.js
+++ force-app/main/default/lwc/nksFagsystemer/nksFagsystemer.js
@@ -41,15 +41,15 @@ export default class NksFagsystemer extends LightningElement {
         { name: 'Foreldrepenger', field: 'NKS_ForeldrepengerURL__c' },
         { name: 'K9', field: 'NKS_K9URL__c' },
         { name: 'Barnetrygd', field: 'NKS_BarnetrygdURL__c' },
-        {
-            name: 'Sosial',
-            field: null,
-            eventFunc: this.handleSosialModiaClickOrKey,
-            title: 'Modia Sosialhjelp',
-            show: NKS_SosialTilgang
-        },
         { name: 'Enslig', field: 'NKS_EnsligForsorgerURL__c' }
     ];
+    // {
+    //     name: 'Sosial',
+    //     field: null,
+    //     eventFunc: this.handleSosialModiaClickOrKey,
+    //     title: 'Modia Sosialhjelp',
+    //     show: NKS_SosialTilgang
+    // },
     // { name: 'SYFO', field: null, eventFunc: this.handleSYFOClickOrKey, title: 'SYFO' },
 
     connectedCallback() {
