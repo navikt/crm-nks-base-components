79e61e8e8247d39ccd95bfacec4a0cf684879fda
Morten Mikalsen <morten.mikalsen@hotmail.com>
2024-04-09 14:04:31 +0200

    Default styling

--- force-app/main/default/lwc/nksPersonHighlightPanelMid/nksPersonHighlightPanelMid.js
+++ force-app/main/default/lwc/nksPersonHighlightPanelMid/nksPersonHighlightPanelMid.js
@@ -1,11 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class NksPersonHighlightPanelMid extends LightningElement {
-    geirs = Array.from(Array(10), (_x, i) => {
-        return {
-            anre: 'Geri' + i
-        };
-    });
-
-    connectedCallback() {}
-}
+export default class NksPersonHighlightPanelMid extends LightningElement {}
