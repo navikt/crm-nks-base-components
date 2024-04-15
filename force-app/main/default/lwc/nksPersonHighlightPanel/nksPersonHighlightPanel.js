089796ef0d14556a6de429f436f1cd977c5bba7b
Jan Robert Gjøstøl <jan.robert.gjostol@nav.no>
2024-04-15 12:17:39 +0200

    refaktorert kode i topp-komponentet

--- force-app/main/default/lwc/nksPersonHighlightPanel/nksPersonHighlightPanel.js
+++ force-app/main/default/lwc/nksPersonHighlightPanel/nksPersonHighlightPanel.js
@@ -3,11 +3,26 @@ import getPersonBadgesAndInfo from '@salesforce/apex/NKS_PersonBadgesController.
 import getPersonAccessBadges from '@salesforce/apex/NKS_PersonAccessBadgesController.getPersonAccessBadges';
 import getHistorikk from '@salesforce/apex/NKS_HistorikkViewController.getHistorikk';
 
+import FULL_NAME_FIELD from '@salesforce/schema/Person__c.CRM_FullName__c';
+import PERSON_IDENT_FIELD from '@salesforce/schema/Person__c.Name';
+import GENDER_FIELD from '@salesforce/schema/Person__c.INT_Sex__c';
+import AGE_FIELD from '@salesforce/schema/Person__c.CRM_Age__c';
+import CITIZENSHIP_FIELD from '@salesforce/schema/Person__c.INT_Citizenships__c';
+import MARITAL_STATUS_FIELD from '@salesforce/schema/Person__c.INT_MaritalStatus__c';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+import getRelatedRecord from '@salesforce/apex/NksRecordInfoController.getRelatedRecord';
+import getNavUnit from '@salesforce/apex/NKS_NavUnitSingleController.findUnit';
+import getNavLinks from '@salesforce/apex/NKS_NavUnitLinks.getNavLinks';
+import { MessageContext, APPLICATION_SCOPE, subscribe, unsubscribe } from 'lightning/messageService';
+import nksVeilederName from '@salesforce/messageChannel/nksVeilderName__c';
+
 export default class NksPersonHighlightPanel extends LightningElement {
     @api recordId;
     @api objectApiName;
     @api relationshipField;
+    personInfo = {};
 
+    wireFields;
     wiredBadge;
     historikkWiredData;
     isLoaded;
@@ -17,6 +32,39 @@ export default class NksPersonHighlightPanel extends LightningElement {
     dateOfDeath;
     badgeContent;
     highlightPanelOpen = false;
+
+    connectedCallback() {
+        this.wireFields = [this.objectApiName + '.Id'];
+        this.subscribeToMessageChannel();
+    }
+
+    @wire(MessageContext)
+    messageContext;
+
+    // Encapsulate logic for Lightning message service subscribe and unsubsubscribe
+    subscribeToMessageChannel() {
+        if (!this.subscription) {
+            this.subscription = subscribe(
+                this.messageContext,
+                nksVeilederName,
+                (message) => this.handleVeilderName(message),
+                { scope: APPLICATION_SCOPE }
+            );
+        }
+    }
+
+    unsubscribeToMessageChannel() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+
+    // Handler for message received by component
+    handleVeilderName(message) {
+        if (message.recordId === this.recordId) {
+            this.veilederName = message.displayName;
+            this.veilederIdent = message.ident;
+        }
+    }
 
     @wire(getPersonBadgesAndInfo, {
         field: '$relationshipField',
@@ -135,5 +183,126 @@ export default class NksPersonHighlightPanel extends LightningElement {
 
     toggleOpen() {
         this.highlightPanelOpen = !this.highlightPanelOpen;
+    }
+
+    // Header Info get data
+
+    getRelatedRecordId(relationshipField, objectApiName) {
+        getRelatedRecord({
+            parentId: this.recordId,
+            relationshipField: relationshipField,
+            objectApiName: objectApiName
+        })
+            .then((record) => {
+                this.personId = this.resolve(relationshipField, record);
+                console.log(this.personId);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    @wire(getRecord, {
+        recordId: '$personId',
+        fields: [FULL_NAME_FIELD, PERSON_IDENT_FIELD, GENDER_FIELD, AGE_FIELD, CITIZENSHIP_FIELD, MARITAL_STATUS_FIELD]
+    })
+    wiredPersonInfo({ error, data }) {
+        if (data) {
+            this.updatePersonInfo('fullName', getFieldValue(data, FULL_NAME_FIELD));
+            this.updatePersonInfo('personIdent', getFieldValue(data, PERSON_IDENT_FIELD));
+            this.updatePersonInfo('gender', getFieldValue(data, GENDER_FIELD));
+            this.updatePersonInfo('age', getFieldValue(data, AGE_FIELD));
+            let __citizenship = getFieldValue(data, CITIZENSHIP_FIELD);
+            if (__citizenship != null && typeof __citizenship === 'string') {
+                this.personInfo.citizenship =
+                    __citizenship.toLowerCase().charAt(0).toUpperCase() + __citizenship.slice(1);
+            } else {
+                this.updatePersonInfo('citizenship', '');
+            }
+
+            let __maritalStatus = getFieldValue(data, MARITAL_STATUS_FIELD);
+            if (__maritalStatus != null && typeof __maritalStatus === 'string') {
+                this.personInfo.maritalStatus = __maritalStatus
+                    .toLowerCase()
+                    .replace(/_/g, ' ')
+                    .replace(' eller enkemann', '/-mann');
+                this.updatePersonInfo(
+                    'maritalStatus',
+                    __maritalStatus.charAt(0).toUpperCase() + __maritalStatus.slice(1)
+                );
+            } else {
+                this.updatePersonInfo('maritalStatus', '');
+            }
+        }
+        if (error) {
+            console.log(error);
+        }
+    }
+
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: '$wireFields'
+    })
+    wiredRecordInfo({ error, data }) {
+        if (data) {
+            if (this.relationshipField && this.objectApiName) {
+                this.getRelatedRecordId(this.relationshipField, this.objectApiName);
+            }
+        }
+        if (error) {
+            console.log(error);
+        }
+    }
+
+    @wire(getNavUnit, {
+        field: '$relationshipField',
+        parentObject: '$objectApiName',
+        parentRecordId: '$recordId',
+        type: 'PERSON_LOCATION'
+    })
+    wiredData(result) {
+        const { data, error } = result;
+        if (data) {
+            this.updatePersonInfo('navUnit', data.unit);
+            this.updatePersonInfo('navUnitName', data.unit ? `${data.unit.enhetNr} ${data.unit.navn}` : '');
+            this.getFormattedLink();
+        }
+        if (error) {
+            console.log(`error: ${error}`);
+        }
+    }
+
+    async getFormattedLink() {
+        if (this.personInfo.navUnit) {
+            // TODO: See if works.
+            const link = await getNavLinks().then((list) => {
+                const onlineCheck = list.find((unit) => unit.enhetNr === this.personInfo.navUnit.unitNr);
+                if (onlineCheck !== undefined) return 'https://www.nav.no' + onlineCheck.path;
+                return (
+                    'https://www.nav.no/kontor/' +
+                    this.personInfo.navUnit.navn
+                        .replace(/\.\s/g, '.')
+                        .replace(/[\s/]/g, '-')
+                        .normalize('NFD')
+                        .replace(/[\u0300-\u036f]/g, '')
+                );
+            });
+            this.updatePersonInfo('formattedUnitLink', link);
+        }
+    }
+
+    updatePersonInfo(field, value) {
+        if (value == null && Object.keys(this.personInfo).includes(field)) return;
+        this.personInfo = { ...this.personInfo, [field]: value };
+    }
+
+    resolve(path, obj) {
+        if (typeof path !== 'string') {
+            throw new Error('Path must be a string');
+        }
+
+        return path.split('.').reduce(function (prev, curr) {
+            return prev ? prev[curr] : null;
+        }, obj || {});
     }
 }
--- force-app/main/default/lwc/nksPersonHighlightPanelTop/nksPersonHighlightPanelTop.js
+++ force-app/main/default/lwc/nksPersonHighlightPanelTop/nksPersonHighlightPanelTop.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class NksPersonHighlightPanelTop extends LightningElement {}
+export default class NksPersonHighlightPanelTop extends LightningElement {
+    @api personInfo;
+    @api badges;
+    openBaba;
+
+    connectedCallback() {
+        console.log(`this.personInfo: ${JSON.stringify(this.personInfo)}`);
+    }
+    renderedCallback() {
+        console.log(`this.personInfo: ${JSON.stringify(this.personInfo)}`);
+    }
+
+    geirArne() {
+        this.openBaba = !this.openBaba;
+    }
+}
