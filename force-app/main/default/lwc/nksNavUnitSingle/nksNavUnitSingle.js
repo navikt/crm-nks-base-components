bc3927788e6ef39948175878a7d18e3e210531ac
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2024-04-18 16:18:51 +0200

    fix error on undefined value of succes + imrove code

--- force-app/main/default/lwc/nksNavUnitSingle/nksNavUnitSingle.js
+++ force-app/main/default/lwc/nksNavUnitSingle/nksNavUnitSingle.js
@@ -9,34 +9,36 @@ import cardLayoutHTML from './cardLayout.html';
 import noLayoutHTML from './noLayout.html';
 
 export default class NksNavUnitSingle extends LightningElement {
-    @api recordId; // The record id
-    @api objectApiName; // The object api name
-    @api relationField; // Points to either the Person__c.Id or a field containging a unit number
-    @api type; // If based on person location or unit
-    @api allSectionsOpenOnLoad = false; // If all sections should be open when the component loads
-    @api numCols = 2; // Number of columns for the displayed fields
-    @api cardLayout = false; // If true, use the card layout, if not use box layout
+    @api recordId;
+    @api objectApiName;
+    @api relationField;
+    @api type;
+    @api allSectionsOpenOnLoad = false;
+    @api numCols = 2;
+    @api cardLayout = false;
     @api boxLayout = false;
 
-    @track navUnit; // The nav unit
-    @track contactInformation; // The nav unit contact information
-    @track contactInformationV2; // The nav unit contact information
+    @track navUnit;
+    @track contactInformation;
+    @track contactInformationV2;
 
-    unitNumber;
-
+    wiredNavUnit;
+    wiredContactInformation;
     wireFields;
-    errorMessage; // Error messages
-    isError = false; // If error has occured
-    isLoaded = false; // If the nav unit and contact information has loaded
-    firstRun = false;
+    errorMessage;
+    isError = false;
+    isLoaded = false;
     noLayout = false;
 
     render() {
-        return this.cardLayout ? cardLayoutHTML : this.boxLayout ? boxLayoutHTML : noLayoutHTML;
+        if (this.cardLayout) {
+            return cardLayoutHTML;
+        }
+        return this.boxLayout ? boxLayoutHTML : noLayoutHTML;
     }
 
     connectedCallback() {
-        this.wireFields = [this.objectApiName + '.Id'];
+        this.wireFields = [`${this.objectApiName}.Id`];
 
         if (!this.cardLayout && !this.boxLayout) {
             this.noLayout = true;
@@ -49,16 +51,15 @@ export default class NksNavUnitSingle extends LightningElement {
     })
     wiredRecordInfo({ error, data }) {
         if (data) {
-            if (this.wiredNavUnit && this.wiredNavUnit.data) {
+            if (this.wiredNavUnit?.data) {
                 this.isLoaded = false;
                 refreshApex(this.wiredNavUnit).then(() => {
                     this.setWiredNavUnit();
                 });
             }
         }
-
         if (error) {
-            this.errorMessage = error;
+            this.errorMessage = error.message || 'Unknown error';
             this.isError = true;
             this.isLoaded = true;
         }
@@ -68,6 +69,9 @@ export default class NksNavUnitSingle extends LightningElement {
         return JSON.stringify(this.errorMessage);
     }
 
+    /**
+     * @param {any} value
+     */
     @wire(getNavUnit, {
         field: '$relationField',
         parentObject: '$objectApiName',
@@ -82,56 +86,58 @@ export default class NksNavUnitSingle extends LightningElement {
     setWiredNavUnit() {
         const { data, error } = this.wiredNavUnit;
         if (data) {
-            let newUnitNumber = data.unit && data.unit.enhetNr ? data.unit.enhetNr : null;
-
-            this.isLoaded = this.unitNumber === newUnitNumber ? true : false;
-
+            let newUnitNumber = data.unit?.enhetNr ? data.unit.enhetNr : null;
+            this.unitNumber = newUnitNumber;
+            this.isLoaded = this.unitNumber === newUnitNumber;
             this.isError = !data.success;
             this.navUnit = data.unit;
-            this.unitNumber = newUnitNumber;
             this.appendErrorMessage(data.errorMessage);
         }
-
         if (error) {
-            this.errorMessage = error;
+            this.errorMessage = error.message || 'Unknown error';
             this.isError = true;
             this.isLoaded = true;
         }
     }
 
-    @wire(getContactInformation, { unitNumber: '$unitNumber' }) wiredGetContactInformation(value) {
-        this.wiredContactInformation = value;
+    /**
+     * @param {any} res
+     */
+    @wire(getContactInformation, { unitNumber: '$unitNumber' }) wiredGetContactInformation(res) {
+        this.wiredContactInformation = res;
         this.setWiredContactInformation();
     }
 
     setWiredContactInformation() {
-        getContactInformationV2({ unitNumber: this.unitNumber }).then((responseV2) => {
-            const { data, error } = this.wiredContactInformation;
-            if (data) {
-                this.isError = !data.success;
-                this.contactInformation = data.contactInformation;
-                this.appendErrorMessage(data.errorMessage);
-            }
-            if (responseV2) {
-                this.isError = !data.success || this.isError;
+        const { data, error } = this.wiredContactInformation;
+        if (data) {
+            this.isError = !data.success;
+            this.contactInformation = data.contactInformation;
+            this.appendErrorMessage(data.errorMessage);
+        } else if (error) {
+            this.errorMessage = error.message || 'Unknown error';
+            this.isError = true;
+        }
 
-                this.contactInformationV2 = responseV2.contactInformation;
-                this.appendErrorMessage(responseV2.errorMessage);
-            }
-
-            if (error) {
-                this.errorMessage = error;
+        getContactInformationV2({ unitNumber: this.unitNumber })
+            .then((res) => {
+                if (res) {
+                    this.contactInformationV2 = res.contactInformation;
+                    this.appendErrorMessage(res.errorMessage);
+                    this.isError = this.isError || !res.success;
+                }
+                this.isLoaded = true;
+            })
+            .catch((err) => {
+                this.errorMessage = err.message || 'Unknown error';
                 this.isError = true;
-            }
-            this.isLoaded = true;
-        });
+                this.isLoaded = true;
+            });
     }
 
     appendErrorMessage(errorMessage) {
-        if (this.errorMessage == null) {
-            this.errorMessage = errorMessage;
-        } else {
-            this.errorMessage += errorMessage ? ' ' + errorMessage : '';
+        if (errorMessage) {
+            this.errorMessage = this.errorMessage ? `${this.errorMessage} ${errorMessage}` : errorMessage;
         }
     }
 
