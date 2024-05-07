acb3489f8662c1591ab0b2928d514bf6da3ab862
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2024-05-07 17:36:14 +0200

    updated article id

--- force-app/main/default/lwc/nksSecurityRoutines/nksSecurityRoutines.js
+++ force-app/main/default/lwc/nksSecurityRoutines/nksSecurityRoutines.js
@@ -2,7 +2,7 @@ import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import OPEN_SECURITY_RUTINES_LABEL from '@salesforce/label/c.NKS_Open_Security_Rutines';
 
-export default class NksSecurityRoutine extends NavigationMixin(LightningElement) {
+export default class NksSecurityRoutines extends NavigationMixin(LightningElement) {
     @api articleId;
 
     openSecurityRutines = OPEN_SECURITY_RUTINES_LABEL;
