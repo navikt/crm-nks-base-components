519ec625f6d39ac5ba23a560bb1d698ebef59ba1
eirikfladby <eirik.fladby@gmail.com>
2024-05-28 14:11:43 +0200

    set styling in mid

--- force-app/main/default/lwc/nksPersonHighlightPanelMid/nksPersonHighlightPanelMid.js
+++ force-app/main/default/lwc/nksPersonHighlightPanelMid/nksPersonHighlightPanelMid.js
@@ -1,5 +1,9 @@
 import { LightningElement, api } from 'lwc';
 
 export default class NksPersonHighlightPanelMid extends LightningElement {
-    @api badgeContent;
+    @api gender;
+
+    get midPanelStyling() { // TODO: Test this - Need .toLowerCase()?
+        return this.gender === 'female' ? 'panel-dark-purple' : 'panel-dark-blue';
+    }
 }
