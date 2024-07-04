e93e9ecbe6fa1ab26fa224ab88b2dee48987afa8
Vegard Sørheller <vegardsorheller@Vegard-sin-MacBook-Pro.local>
2024-07-04 10:05:15 +0200

    Adding support for success messages when journal already exists

--- force-app/main/default/lwc/nksButtonContainerUtils/nksButtonContainerUtils.js
+++ force-app/main/default/lwc/nksButtonContainerUtils/nksButtonContainerUtils.js
@@ -31,10 +31,13 @@ export async function handleShowNotifications(
     try {
         const flowNameLower = flowName.toLowerCase();
         const selectedThemeId = getOutputVariableValue(outputVariables, 'Selected_Theme_SF_Id');
+        const exsistinJorunal = getOutputVariableValue(outputVariables, 'Existing_Journal');
         const theme = selectedThemeId ? await callGetCommonCode(selectedThemeId) : '';
 
         if (flowNameLower.includes('journal')) {
-            const message = journalConversationNote
+            const message = exsistinJorunal
+                ? 'Henvendelsen er allerede jounralført'
+                : journalConversationNote
                 ? 'Samtalereferat er delt med bruker og henvendelsen er journalført'
                 : 'Henvendelsen er journalført';
             addNotification(notificationBoxTemplate, message, theme);
