9857418b300b00dc18bdc6186ec2de893a9e8687
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2024-09-19 14:08:09 +0200

    added function to filter notification list

--- force-app/main/default/lwc/nksNotificationBox/nksNotificationBox.js
+++ force-app/main/default/lwc/nksNotificationBox/nksNotificationBox.js
@@ -15,6 +15,13 @@ export default class NksNotificationBox extends LightningElement {
         });
     }
 
+    @api
+    filterNotification(inputText) {
+        this.notificationList = this.notificationList.filter(
+            (item) => !item.text.toLowerCase().includes(inputText.toLowerCase())
+        );
+    }
+
     getCurrentTime() {
         const nowTime = new Date();
         return `${('0' + nowTime.getHours()).slice(-2)}:${('0' + nowTime.getMinutes()).slice(-2)}`;
