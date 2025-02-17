a98aea41412f8f14fe62dfaae6752ed2aafd9fc4
eirikfladby <eirik.fladby@gmail.com>
2025-02-17 10:48:47 +0100

    added wrapper class and todos

--- force-app/main/default/lwc/nksRecordPrintButton/nksRecordPrintButton.js
+++ force-app/main/default/lwc/nksRecordPrintButton/nksRecordPrintButton.js
@@ -3,7 +3,8 @@ import { NavigationMixin } from 'lightning/navigation';
 
 export default class NksRecordPrintButton extends NavigationMixin(LightningElement) {
     @api recordId;
-
+    // TODO: Decide where button should go on pages (Probably on the conv note / thread / chat / utbetaling pages instead of header)
+    // TODO: This component can be deleted and instead the custom nks-button component can be used with this handlePrint method on onclick()
     handlePrint() {
         console.log(this.recordId);
         if (!this.recordId) {
