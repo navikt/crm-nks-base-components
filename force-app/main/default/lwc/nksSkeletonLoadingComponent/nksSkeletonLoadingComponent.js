bf380fa45382f47a303fc5195491b5748b6c2b4a
eirikfladby <eirik.fladby@gmail.com>
2025-03-11 14:29:50 +0100

    styling for unknown personid on skeleton

--- force-app/main/default/lwc/nksSkeletonLoadingComponent/nksSkeletonLoadingComponent.js
+++ force-app/main/default/lwc/nksSkeletonLoadingComponent/nksSkeletonLoadingComponent.js
@@ -1,18 +1,17 @@
 import { LightningElement, api } from 'lwc';
-import personHighlightPanel from './personHighlightPanel.html';
+import NAV_ICONS from '@salesforce/resourceUrl/NKS_navIcons';
 
 export default class SkeletonLoadingComponent extends LightningElement {
-    @api personId = '123'; // Default to true, will be overwritten once parent is done with wires
-
-    render() {
-        return personHighlightPanel;
-    }
+    @api personId = '123'; // Default to true to show loading, will be overwritten once parent is done with wires
 
     get isPersonIdSet() {
         console.log(this.personId);
         return this.personId !== null;
     }
-}
 
-// INFO: This component is shown until parent has set personid from wires
-// TODO: Set color and correct height according to highlightpanel (height is off atm)
+    get unknownIcon() {
+        return NAV_ICONS + '/' + 'confidentialCircleFilled' + '.svg#' + 'confidentialCircleFilled';
+    }
+}
+// TODO: Test with chats
+// TODO: Test with Puzzel no person id
