8a302c8394ca2a03982035073869b245108e0b37
Sara Mohammadi <86772218+saramoh2309@users.noreply.github.com>
2026-08-06 08:53:42 +0200

    udpated contact information for nav unit

--- force-app/main/default/lwc/nksNavUnitContactInformation/nksNavUnitContactInformation.js
+++ force-app/main/default/lwc/nksNavUnitContactInformation/nksNavUnitContactInformation.js
@@ -3,56 +3,40 @@ import { LightningElement, api } from 'lwc';
 export default class NksNavUnitContactInformation extends LightningElement {
     @api numCols = 2;
 
-     _contactInformation;
-     _visitorLocations = [];
+    _contactInformation;
+    _visitorLocations = [];
     _visitorLocationsLength = 0;
     hasContactInformation = false;
     hasVisitorLocations = false;
 
     @api
     set contactInformation(value) {
-        if (value) {
-            this._contactInformation = value;
-            this.hasContactInformation = true;
+        this._contactInformation = value || null;
+        this.hasContactInformation = !!value;
 
-            const publikumsmottak = value.brukerkontakt?.publikumsmottak ?? [];
-            if (publikumsmottak.length) {
-                this.hasVisitorLocations = true;
-                this._visitorLocations = publikumsmottak;
-                this._visitorLocationsLength = publikumsmottak.length;
-            }
-        }
+        const publikumsmottak = value?.brukerkontakt?.publikumsmottak ?? [];
+        this._visitorLocations = publikumsmottak;
+        this._visitorLocationsLength = publikumsmottak.length;
+        this.hasVisitorLocations = publikumsmottak.length > 0;
+    }
+
+    get contactInformation() {
+        return this._contactInformation;
     }
 
     get columnWidth() {
         return 12 / this.numCols;
     }
 
-    //get hasContactInformation() { return this._contactInformation ? true : false; }
-    get contactInformation() {
-        return this._contactInformation;
-    }
-
-    //get hasVisitorLocations() { return 0 < this._visitorLocationsLength; }
     get visitorLocations() {
         return this._visitorLocations;
     }
 
     get postalAddress() {
-        const address = this.contactInformation.postadresse;
-        if (!address) return '';
-
-        const { postboksnummer, postnummer, poststed } = address;
-
-        return `Postboks ${postboksnummer}, ${postnummer} ${poststed}`;
+        return this.contactInformation?.postadresse?.concatenatedAddress || '';
     }
 
     get visitingAddress() {
-        const address = this.contactInformation.besoeksadresse;
-        if (!address) return '';
-
-        const { gatenavn, husnummer, postnummer, poststed } = address;
-
-        return `${gatenavn} ${husnummer}, ${postnummer} ${poststed}`;
+        return this.contactInformation?.besoeksadresse?.concatenatedAddress || '';
     }
 }
