df0d315adc1793f4c722ea0481754d356c50cf1a
Sara Mohammadi <86772218+saramoh2309@users.noreply.github.com>
2026-08-07 09:54:09 +0200

    fix test + use same field for visiting adress for visiterLocation

--- force-app/main/default/lwc/nksNavUnitVisitorLocation/nksNavUnitVisitorLocation.js
+++ force-app/main/default/lwc/nksNavUnitVisitorLocation/nksNavUnitVisitorLocation.js
@@ -3,12 +3,7 @@ export default class NksNavUnitVisitorLocation extends LightningElement {
     @api location;
 
     get visitingAddress() {
-        const address = this.location.besoeksadresse;
-        if (!address) return '';
-
-        const { gatenavn, husnummer, postnummer, poststed } = address;
-
-        return `${gatenavn} ${husnummer}, ${postnummer} ${poststed}`;
+        return this.location?.besoeksadresse?.concatenatedAddress || '';
     }
 
     get locationName() {
